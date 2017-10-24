6dbdc0c129bbe0676a9386656bf8ee3790bf2890
Akshay Gaikwad <akgaikwad001@gmail.com>
2017-10-24 08:39:16 +0530

    Add ViewFeed Activity

---
+++ app/src/main/java/in/geeksocket/server/feedreader/ViewFeedActivity.java
@@ -0,0 +1,13 @@
+package in.geeksocket.server.feedreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_feed);
+    }
+}
