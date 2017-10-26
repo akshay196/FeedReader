98e9b6e70360dab77f759d79e4081db12bb69528
Akshay Gaikwad <akgaikwad001@gmail.com>
2017-10-26 10:11:59 +0530

    Get url data in background

--- app/src/main/java/in/geeksocket/server/feedreader/MainActivity.java
+++ app/src/main/java/in/geeksocket/server/feedreader/MainActivity.java
@@ -1,10 +1,21 @@
 package in.geeksocket.server.feedreader;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Parameter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void moveToView(View view) {
+        new GetData().execute();
+
         Intent intent = new Intent(this, ViewFeedActivity.class);
         EditText text = (EditText) findViewById(R.id.editText2);
         String url = text.getText().toString();
@@ -23,3 +36,49 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
+class GetData extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... strings){
+        HttpURLConnection urlConnection = null;
+        String result = "";
+        try{
+            URL url = new URL("https://www.google.com");
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            int code = urlConnection.getResponseCode();
+
+            if(code==200){
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                if (in != null) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+                    String line = "";
+
+                    while ((line = bufferedReader.readLine()) != null)
+                        result += line;
+                }
+                in.close();
+            }
+
+
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        System.out.println(result);
+    }
+
+}
+
